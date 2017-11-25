fcb3ba2a319ce803fe9b46903d1222f6996985a4
Josh Foster <josh@allenb.com>
2017-11-24 21:03:19 -0600

    made slider have transition delay on first mouseenter only, then instant transition when sliding betweenother slider items, also added div for elapsed time that is not enlarged on mouseenter

--- index.js
+++ index.js
@@ -11,25 +11,60 @@ const movies = {
   7:'pink'
 }
 
-$('.movie-glimpse').on('mouseenter mouseleave',(event)=>{
-  let $target=$(event.target);
+let hovering = false;
+$('.movie-slider').on('mouseenter mouseleave',(event)=>{
   if(event.type==='mouseenter'){
-    if($target.is('.movie-glimpse:first-child')){
-      $('.movie-slider').addClass('go_right');
-    }else if($target.is('.movie-glimpse:last-child')){
-      $('.movie-slider').addClass('go_left');
-    }
-    $target.addClass('big');
-    $target.prevAll().addClass('go_left');
-    $target.nextAll().addClass('go_right');
+    setTimeout(()=>{
+      $('.movie-glimpse').addClass('hovering');
+      hovering=true;
+    },350);
   }else if(event.type==='mouseleave'){
-    if($target.is('.movie-glimpse:first-child')){
-      $('.movie-slider').removeClass('go_right');
-    }else if($target.is('.movie-glimpse:last-child')){
-      $('.movie-slider').removeClass('go_left');
+    // $('.movie-glimpse').removeClass('hovering');
+    setTimeout(()=>{
+      $('.movie-glimpse').removeClass('hovering');
+      hovering=false;
+    },350);
+  }
+});
+
+const move_right = ($target,event) =>{
+  if($target.is('.movie-glimpse:first-child')){
+    $('.movie-slider').addClass('go_right');
+  }else if($target.is('.movie-glimpse:last-child')){
+    $('.movie-slider').addClass('go_left');
+  }
+  $(event.target).closest('.glimpse').addClass('big');
+  $target.prevAll().addClass('go_left');
+  $target.nextAll().addClass('go_right');
+}
+const move_left = ($target,event) =>{
+  if($target.is('.movie-glimpse:first-child')){
+    $('.movie-slider').removeClass('go_right');
+  }else if($target.is('.movie-glimpse:last-child')){
+    $('.movie-slider').removeClass('go_left');
+  }
+  $(event.target).closest('.glimpse').removeClass('big');
+  $target.prevAll().removeClass('go_left');
+  $target.nextAll().removeClass('go_right');
+}
+$('.movie-glimpse').on('mouseenter mouseleave',(event)=>{
+  let $target=$(event.target).closest('.movie-glimpse');
+  if(event.type==='mouseenter'){
+    if(hovering===true){
+      move_right($target,event);
+    }else{
+      setTimeout(()=>{
+        move_right($target,event);
+      },350);
     }
-    $target.removeClass('big');
-    $target.prevAll().removeClass('go_left');
-    $target.nextAll().removeClass('go_right');
+  }else if(event.type==='mouseleave'){
+    if(hovering===true){
+      move_left($target,event);
+    }else{
+      setTimeout(()=>{
+        move_left($target,event);
+      },350);
+    }
+
   }
 });
